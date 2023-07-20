5274820671072cd608d0d336df4fa86f73b40e5a
ruchikxxii <ruchik710@gmail.com>
2023-07-21 00:33:08 +0530

    fixed getQuestion

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const router = require('./routes')
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api",router);
 
 app.listen(5000,()=>{
--- backend/utilities.js
+++ backend/utilities.js
@@ -4,7 +4,7 @@ const getRandomSentences = (x) => {
     var l = randomSentences.length-1;
     var ret = "";
     for(i=0;i<x;i++){
-        r = Math.random()*l;
+        r = Math.floor(Math.random()*l);
         ret += " "+randomSentences[r];
     }
     return ret;
