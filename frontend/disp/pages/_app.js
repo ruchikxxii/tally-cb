d3c38d676a422b35ab1564f1122f4b7941bdbd11
Sharad Arora <f20211427@goa.bits-pilani.ac.in>
2023-07-21 00:14:08 +0530

    add navbar

--- frontend/disp/pages/_app.js
+++ frontend/disp/pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import Layout from "@/components/layout";
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Layout>
+
       <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
