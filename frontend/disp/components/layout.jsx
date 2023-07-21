028b24d4b6a89ad14dddc16428db7bc944261c0f
Sharad Arora <f20211427@goa.bits-pilani.ac.in>
2023-07-21 15:37:59 +0530

    add socket context

--- frontend/disp/components/layout.jsx
+++ frontend/disp/components/layout.jsx
@@ -1,12 +1,14 @@
 import {React} from "react";
 import Navbar from "./navbar";
-
+import { SocketContext,socket } from "@/context/socket";
 export default function Layout({children}){
     return(
         
         <>
+        <SocketContext.Provider value={socket}>
         <Navbar/>
         {children}
+        </SocketContext.Provider>
         </>
     );
 }
--- frontend/disp/pages/index.js
+++ frontend/disp/pages/index.js
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const router = useRouter();
-  function changePage() {
-    router.push("/test");
-  }
+
+
   return (
     <div className="page my-16 text-white">
       <section className="hero-section">
