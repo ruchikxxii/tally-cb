cc7b653aa35f4eeceaaaa551c08c66041a8b4973
Sharad Arora <f20211427@goa.bits-pilani.ac.in>
2023-07-22 12:04:13 +0530

    attempt to fix merge error

--- frontend/disp/components/joinForm.jsx
+++ frontend/disp/components/joinForm.jsx
@@ -12,24 +12,25 @@ import {
   ModalFooter
 } from "@chakra-ui/react";
 import {GiAssassinPocket} from 'react-icons/gi'
-import { useEffect,useContext,useRef } from 'react'
+import { useEffect,useContext } from 'react'
 import { SocketContext } from '@/context/socket'
 import { gameContext } from '@/context/room';
 import { FormLabel } from "react-bootstrap";
 import{ useRouter }from "next/router";
 export default function JoinForm({openJoinForm,closeJoinForm}){
-  const roomval=useRef(null);
   const router=useRouter();
   const socket=useContext(SocketContext);
-  const {inRoom,setInRoom,setRoomName,setUsername,setQuestion,setTimer,setSpeed}=useContext(gameContext);
+  const {inRoom,setInRoom,setRoomName,setUsername,setQuestion,setTimer,setPlayers,setCanJoin}=useContext(gameContext);
   useEffect(()=>{
     socket.on('join room status',(status)=>{
+      console.log(status)
       if(status.success){
         setInRoom(true);
-        setRoomName(roomval.current.value);
-        setTimer(status.time);
-        setSpeed(status.speed);
+        setRoomName(status.room_name);
         setQuestion(status.question);
+        setTimer(status.time);
+        setPlayers(2);
+        setCanJoin(status.canJoin);
         router.push('/typing_room');
       }
     })
@@ -58,7 +59,7 @@ export default function JoinForm({openJoinForm,closeJoinForm}){
           <FormLabel>Enter Your Name</FormLabel>
             <Input isRequired name="name" onChange={formikss.handleChange} value={formikss.values.name} style={{backgroundColor:"#EDE4FF",color:"black"}} placeholder="Enter Name Here"/>
             <FormLabel>Enter Room Code</FormLabel>
-            <Input ref={roomval} isRequired name="room_name" onChange={formikss.handleChange} value={formikss.values.room_name} style={{backgroundColor:"#EDE4FF",color:"black"}} placeholder="Enter Code Here"/>
+            <Input isRequired name="room_name" onChange={formikss.handleChange} value={formikss.values.room_name} style={{backgroundColor:"#EDE4FF",color:"black"}} placeholder="Enter Code Here"/>
           </ModalBody>
 
           <ModalFooter>
